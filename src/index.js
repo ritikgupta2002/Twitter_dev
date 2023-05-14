5301b3978cbf7d0799c6926403f05552db2942d7
ritik <gupta.ritik2002@gmail.com>
2023-05-14 15:09:42 +0530

    Added v1 for tweet model and hashtag model

--- src/index.js
+++ src/index.js
@@ -2,23 +2,8 @@ const express = require("express");
 const connect = require("./config/database");
 const app = express();
 
-const TweetRepository = require("./repository/tweet-repository");
-const Comment = require("./models/comment");
-
 app.listen(3000, async () => {
   console.log("server started");
   await connect();
   console.log("Mongodb connected");
-  const tweetRepo = new TweetRepository();
-  // const tweet = await tweetRepo.create({
-  //   content: "Tweet with comment schema2",
-  // });
-  // console.log(tweet);
-  // const comment = await Comment.create({ content: "new comment2" });
-  // tweet.comments.push(comment);
-  // await tweet.save();
-  const tweet=await tweetRepo.create({content:"with hooks now "});
-  console.log(tweet);
-  // const tweet= await tweetRepo.getAll(2,4);
-  // console.log(tweet[0].contentWithEmail);   
 });
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -5,32 +5,17 @@ const tweetSchema = new mongoose.Schema(
     content: {
       type: String,
       required: true,
+      max: [250, "Tweet cannot be more than 250 characters"],
     },
-    userEmail: {
-      type: String,
-    },
-    comments: [
+    hashtags: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Comment",
+        ref: "Hashtag",
       },
     ],
   },
   { timestamps: true }
 );
 
-//schema is a blueprint , model is a actual object that will create the data for us .
-tweetSchema.virtual('contentWithEmail').get(function process(){
-  return `${this.content} \nCreated by: ${this.userEmail}`;
-})
-
-tweetSchema.pre('save',function(next){
-  console.log('Inside a hook ');
-  this.content=this.content+'......'
-  next();
-})
-
-//virtual helps us to write custom queries , we can define any with the help of virtuals which will be 
-//computed at runtime and may not be a part of the actual schema .
 const Tweet = mongoose.model("Tweet", tweetSchema);
 module.exports = Tweet;
