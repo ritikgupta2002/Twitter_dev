713a0ca6fd789a722e300e85117d0289b3677cc5
ritik <gupta.ritik2002@gmail.com>
2023-05-21 16:45:06 +0530

    Moved to es modules

--- src/models/hashtags.js
+++ src/models/hashtags.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
 
 const hashtagSchema = new mongoose.Schema(
   {
@@ -16,4 +16,5 @@ const hashtagSchema = new mongoose.Schema(
   { timestamps: true }
 );
 const Hashtag = mongoose.model("Hashtag", hashtagSchema);
-module.exports = Hashtag;
+
+export default Hashtag;
--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,4 +1,4 @@
-const Tweet = require("../models/tweet");
+import Tweet from '../models/tweet.js';
 
 class TweetRepository {
   async create(data) {
@@ -27,14 +27,6 @@ class TweetRepository {
     }
   }
 
-  // async update(tweetId, data) {
-  //   try {
-  //     const tweet = await Tweet.findByIdAndUpdate(tweetId, data, { new: true });
-  //     return tweet;
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
   async destroy(id) {
     try {
       const tweet = await Tweet.findByIdAndRemove(id);
@@ -54,5 +46,4 @@ class TweetRepository {
   } 
 }
 
-module.exports = TweetRepository;
-//1:27:23
+export default TweetRepository;
