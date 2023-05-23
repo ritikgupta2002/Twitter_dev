8ce0ecd623ea6a3aacf83b0d83c680e99951f99a
ritik <gupta.ritik2002@gmail.com>
2023-05-23 21:39:20 +0530

    Added basic crud repo and extended it in likerepo

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,6 +1,10 @@
-import Tweet from '../models/tweet.js';
+import Tweet from "../models/tweet.js";
+import CrudRepository from "./crud-repository.js";
 
-class TweetRepository {
+class TweetRepository extends CrudRepository {
+  constructor() {
+    super(Tweet);
+  }
   async create(data) {
     try {
       const tweet = await Tweet.create(data);
@@ -9,41 +13,26 @@ class TweetRepository {
       console.log(error);
     }
   }
-  async get(id) {
-    try {
-      const tweet = await Tweet.findById(id);
-      return tweet;
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   async getWithComments(id) {
     try {
-      const tweet = await Tweet.findById(id).populate({path:"comments"}).lean();
+      const tweet = await Tweet.findById(id)
+        .populate({ path: "comments" })
+        .lean();
       return tweet;
     } catch (error) {
       console.log(error);
     }
   }
 
-  async destroy(id) {
-    try {
-      const tweet = await Tweet.findByIdAndRemove(id);
-      return tweet;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async getAll(offset,limit){
+  async getAll(offset, limit) {
     try {
       const tweet = await Tweet.find().skip(offset).limit(limit);
       return tweet;
     } catch (error) {
       console.log(error);
     }
-  } 
+  }
 }
 
 export default TweetRepository;
